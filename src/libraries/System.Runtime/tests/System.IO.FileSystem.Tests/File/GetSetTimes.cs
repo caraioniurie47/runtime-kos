182bbf5b019348365fc42843dd673d2a435eea01
Iurie Caraion <caraioniurie47@gmail.com>
2026-09-26 09:03:41 +0300

    Tests: skip the synthesized creation time test on KOS

--- src/libraries/System.Runtime/tests/System.IO.FileSystem.Tests/File/GetSetTimes.cs
+++ src/libraries/System.Runtime/tests/System.IO.FileSystem.Tests/File/GetSetTimes.cs
@@ -73,7 +73,8 @@ namespace System.IO.Tests
             Assert.InRange(GetCreationTimeUtc(path), DateTime.MinValue, GetLastWriteTimeUtc(path));
         }
 
-        [Fact]
+        // KasperskyOS stores a birth time, which setting the times does not change, so CreationTime is not synthesized
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNotKasperskyOS))]
         [PlatformSpecific(TestPlatforms.Linux)]
         public async Task CreationTimeSet_GetReturnsExpected_WhenNotInFuture()
         {

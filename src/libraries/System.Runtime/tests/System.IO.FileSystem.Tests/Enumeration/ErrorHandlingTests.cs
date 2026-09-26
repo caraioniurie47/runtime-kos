266b06d3660ff64bb33412e98cc15c23c116c1f8
Iurie Caraion <caraioniurie47@gmail.com>
2026-09-26 09:03:30 +0300

    Tests: skip enumerating a removed directory on KOS

--- src/libraries/System.Runtime/tests/System.IO.FileSystem.Tests/Enumeration/ErrorHandlingTests.cs
+++ src/libraries/System.Runtime/tests/System.IO.FileSystem.Tests/Enumeration/ErrorHandlingTests.cs
@@ -81,7 +81,7 @@ namespace System.IO.Tests
             }
         }
 
-        [Fact]
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNotKasperskyOS))] // KasperskyOS: readdir on a removed directory fails with ENOENT (allowed by POSIX) instead of ending the stream
         [ActiveIssue("https://github.com/dotnet/runtime/issues/55821", TestPlatforms.Android)]
         public void DeleteDirectoryAfterOpening()
         {
